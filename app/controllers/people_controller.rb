30dd98b043c0636d007bdb05fb886582aa5096fe
Samuel Esposito <s.esposito@med.umcg.nl>
2010-10-09 12:14:58 +0200

    add login field

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -2,7 +2,7 @@ class PeopleController < ApplicationController
   
   def login
     unless params[:user_name].blank? or params[:password].blank?
-      if person = Person.authenticate params[:user_name], params[:password]
+      if person = Person.authenticate(params[:user_name], params[:password])
         session[:person_id] = person.id
       end
     end
