0688dfc5a827eacc4e5a31eb0d8f1122066fb86d
Samuel Esposito <s.esposito@med.umcg.nl>
2010-10-09 12:16:52 +0200

    add brute force protection

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,11 +1,13 @@
 class PeopleController < ApplicationController
   
   def login
+    session.delete :person_id
     unless params[:user_name].blank? or params[:password].blank?
       if person = Person.authenticate(params[:user_name], params[:password])
         session[:person_id] = person.id
       end
     end
+    sleep 5 unless session[:person_id]
     redirect_to :back
   end
   
