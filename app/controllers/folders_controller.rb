22ea73cf96073c093d642c91dcd4aaf57b29fec8
Samuel Esposito <s.esposito@med.umcg.nl>
2010-10-11 01:03:57 +0200

    minor tweak

--- app/controllers/folders_controller.rb
+++ app/controllers/folders_controller.rb
@@ -4,7 +4,7 @@ class FoldersController < ApplicationController
     path = hash_for_source_files_path.merge({:folder_id => params[:parent_id]})
     if @person
       folder = Folder.create params
-      flash[:success] = "Folder #{folder.name} added." if folder.errors.blank?
+      flash[:success] = "Folder \"#{folder.name}\" added." if folder.errors.blank?
       redirect_to path.merge({:selected_id => folder.id})
     else
       redirect_to path
@@ -14,9 +14,9 @@ class FoldersController < ApplicationController
   def destroy
     folder = Folder.find params[:id]
     if folder.destroy_folder
-      flash[:success] = "Folder #{folder.name} removed."
+      flash[:success] = "Folder \"#{folder.name}\" removed."
     else
-      flash[:message] = "Folder #{folder.name} couldn't be removed."
+      flash[:message] = "Folder \"#{folder.name}\" couldn't be removed."
     end
     redirect_to :back
   end
