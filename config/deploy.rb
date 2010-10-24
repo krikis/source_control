1ce1c48cce371d8bf6d6e6c4f469c99f5dfd0827
Samuel Esposito <s.esposito@med.umcg.nl>
2010-10-24 13:05:55 +0200

    capify project

---
+++ config/deploy.rb
@@ -0,0 +1,41 @@
+set :application, "source_control"
+set :repository,  "git@github.com/krikis:source_control.git"
+
+set :scm, :git
+set :deploy_via, :remote_cache
+
+set :user, "root"
+set :use_sudo, true
+
+set :appserver, "192.168.1.35"
+
+role :web, appserver                          # Your HTTP server, Apache/etc
+role :app, appserver                          # This may be the same as your `Web` server
+role :db,  appserver, :primary => true # This is where Rails migrations will run
+
+namespace :bundler do
+  task :create_symlink, :roles => :app do
+    shared_dir = File.join(shared_path, 'bundle')
+    release_dir = File.join(current_release, '.bundle')
+    run("mkdir -p #{shared_dir} && ln -s #{shared_dir} #{release_dir}")
+  end
+ 
+  task :bundle_new_release, :roles => :app do
+    bundler.create_symlink
+    run "cd #{release_path} && bundle install --without test"
+  end
+end
+ 
+after 'deploy:update_code', 'bundler:bundle_new_release'
+
+# If you are using Passenger mod_rails uncomment this:
+# if you're still using the script/reapear helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
