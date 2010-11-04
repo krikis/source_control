352a59af8764938e6ad509ad10e756204afed668
Samuel Esposito <s.esposito@med.umcg.nl>
2010-11-04 23:10:26 +0100

    add unit tests

--- config/environment.rb
+++ config/environment.rb
@@ -5,4 +5,4 @@ require File.expand_path('../application', __FILE__)
 SourceControl::Application.initialize!
 
 # sass file settings
-Sass::Plugin.add_template_location "#{RAILS_ROOT}/app/views"
+Sass::Plugin.add_template_location "#{Rails.root}/app/views"
--- test/unit/source_file_test.rb
+++ test/unit/source_file_test.rb
@@ -1,8 +1,33 @@
 require 'test_helper'
 
 class SourceFileTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  
+  test "creating a source file without a name" do
+    sf = SourceFile.create
+    assert (not sf.valid?)
+    assert sf.errors.first == [:name, "can't be blank"]
+    assert SourceFile.find_by_folder_id_and_name("", "") == nil
+  end
+
+  test "creating a source file with a name" do
+    source_file = SourceFile.find_by_folder_id_and_name("", "my_test_file.rb")
+    source_file.destroy if source_file
+    sf = SourceFile.create :name => "my_test_file.rb", :folder_id => ""
+    assert sf.valid?
+    assert sf.errors.empty?
+    assert sf.id
+    assert source_file = SourceFile.find_by_folder_id_and_name("", "my_test_file.rb")
+  end
+
+  test "creating a source file with an existing name" do
+    source_file = SourceFile.find_by_folder_id_and_name("", "my_test_file.rb")
+    source_file.destroy if source_file
+    sf = SourceFile.create :name => "my_test_file.rb", :folder_id => ""
+    assert sf.valid?
+    assert sf.errors.empty?
+    assert sf.id
+    assert source_file = SourceFile.find_by_folder_id_and_name("", "my_test_file.rb")
+    sf2 = SourceFile.create :name => "my_test_file.rb", :folder_id => ""
+    assert (not sf2.id)
   end
 end
